cd8cc6c75b30cb9463f08fde89b797b07933ce18
Stricks1 <gmalheiross@gmail.com>
2020-07-01 15:16:27 -0300

    add user and login/singup/logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   resources :users
   resources :opinions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'opinions#index'
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 end
