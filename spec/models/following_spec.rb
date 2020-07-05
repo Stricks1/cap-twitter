1942231e87211b69b9f563a5b6350d5ad0bd7212
Stricks1 <gmalheiross@gmail.com>
2020-07-05 01:36:39 -0300

    fixing linter element identation problem

--- spec/models/following_spec.rb
+++ spec/models/following_spec.rb
@@ -7,19 +7,22 @@ RSpec.describe User, type: :model do
       User.reset_pk_sequence
       Following.destroy_all
       Following.reset_pk_sequence
-      User.create!([
-      {
-        username: 'stricks',
-        full_name: 'Gabriel Silveira',
-        photo: 'https://avatars3.githubusercontent.com/u/60522356?s=400&u=c26966471d76a25d871027b43c0282504e04caca&v=4',
-        cover_image: 'https://www.shutterstock.com/blog/wp-content/uploads/sites/5/2017/08/nature-design.jpg'
-      },
-      {
-        username: 'Harry',
-        full_name: 'Harry Potter',
-        photo: 'https://veja.abril.com.br/wp-content/uploads/2016/09/harrypotterrotter-1.jpg',
-        cover_image: 'https://i.pinimg.com/originals/c2/4b/e8/c24be8b914079df7aad2e3fb267d40f7.jpg'
-      }])
+      User.create!(
+        [
+          {
+            username: 'stricks',
+            full_name: 'Gabriel Silveira',
+            photo: 'https://avatars3.githubusercontent.com/u/60522356?s=400&u=c26966471d76a25d871027b43c0282504e04caca&v=4',
+            cover_image: 'https://www.shutterstock.com/blog/wp-content/uploads/sites/5/2017/08/nature-design.jpg'
+          },
+          {
+            username: 'Harry',
+            full_name: 'Harry Potter',
+            photo: 'https://veja.abril.com.br/wp-content/uploads/2016/09/harrypotterrotter-1.jpg',
+            cover_image: 'https://i.pinimg.com/originals/c2/4b/e8/c24be8b914079df7aad2e3fb267d40f7.jpg'
+          }
+        ]
+      )
     end
 
     it 'create a new follow between users' do
