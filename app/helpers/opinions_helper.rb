3411efca311ccda1e2a8dd6752247b480925bd3f
Stricks1 <gmalheiross@gmail.com>
2020-07-05 00:37:38 -0300

    fixing linters on helpers

--- app/helpers/opinions_helper.rb
+++ app/helpers/opinions_helper.rb
@@ -1,46 +1,37 @@
 module OpinionsHelper
   def right_title
-    if current_user
-      cntnt = 'WHO TO FOLLOW'
-    else 
-      cntnt = 'WHO IS HERE'
-    end
+    current_user ? 'WHO TO FOLLOW' : 'WHO IS HERE'
   end
 
   def center_title
-    if current_user
-      cntnt = 'Followeds Opinions'
-    else
-      cntnt = 'All Opinions'
-    end
+    current_user ? 'Followeds Opinions' : 'All Opinions'
   end
 
   def linked_unlinked_comment(opi)
     if current_user
-      cntnt = link_to image_tag(opi.user.photo, border: 0, class: 'timeline-photo'), user_path(opi.user)
+      link_to image_tag(opi.user.photo, border: 0, class: 'timeline-photo'), user_path(opi.user)
     else
-      cntnt = image_tag(opi.user.photo, border: 0, class: 'timeline-photo')
+      image_tag(opi.user.photo, border: 0, class: 'timeline-photo')
     end
   end
 
-
-  def linked_unlinked_who(u)
+  def linked_unlinked_who(usr)
     if current_user
-      cntnt = link_to image_tag(u.photo, border: 0, class: 'rounded-circle who-photo mx-2 align-self-center'), user_path(u), class: 'align-self-center'
+      link_to image_tag(usr.photo, border: 0, class: 'rounded-circle who-photo mx-2 align-self-center'), user_path(usr), class: 'align-self-center'
     else
-      cntnt = image_tag(u.photo, border: 0, class: 'rounded-circle who-photo mx-2 align-self-center')
+      image_tag(usr.photo, border: 0, class: 'rounded-circle who-photo mx-2 align-self-center')
     end
   end
 
   def edit_destroy_retweet(opi)
     cntnt = ''
     if current_user && opi.user.username == current_user.username
-      cntnt.concat(link_to image_tag('https://raw.githubusercontent.com/Stricks1/cap-twitter/b3ee28c00f9504f9995a544ad14f2440b83f40a0/app/assets/images/pencil.svg', border: 0, class: 'edit-opinion'), edit_opinion_path(opi))
-      cntnt.concat(link_to 'x', opi, method: :delete, data: { confirm: 'Are you sure?' }, :class => 'textdec-none ml-1')
+      cntnt.concat((link_to image_tag('https://raw.githubusercontent.com/Stricks1/cap-twitter/b3ee28c00f9504f9995a544ad14f2440b83f40a0/app/assets/images/pencil.svg', border: 0, class: 'edit-opinion'), edit_opinion_path(opi)))
+      cntnt.concat((link_to 'x', opi, method: :delete, data: { confirm: 'Are you sure?' }, class: 'textdec-none ml-1'))
       cntnt.html_safe
     else
       if current_user && opi.user.username != current_user.username
-        cntnt.concat(link_to image_tag('https://raw.githubusercontent.com/Stricks1/cap-twitter/feature/app/assets/images/retweet.png', border: 0, class: 'edit-opinion'), retweet_path(opi))
+        cntnt.concat((link_to image_tag('https://raw.githubusercontent.com/Stricks1/cap-twitter/feature/app/assets/images/retweet.png', border: 0, class: 'edit-opinion'), retweet_path(opi)))
         cntnt.html_safe
       end
     end
@@ -49,18 +40,18 @@ module OpinionsHelper
   def copied_info(opi)
     return nil unless opi.copied
 
-    edited = opi.created_at == opi.updated_at ? false : true
+    edited = !(opi.created_at == opi.updated_at)
     info = edited ? 'Edited opinion copied from @' : 'Opinion copied from @'
     info.concat(opi.copied.username)
     if current_user
-      cntnt = link_to info, user_path(opi.copied)
+      link_to info, user_path(opi.copied)
     else
-      cntnt = info
+      info
     end
   end
 
-  def op_btn_follow(u)
-    link = link_to '+', follow_path(u), :class => 'textdec-none circle-link'
+  def op_btn_follow(usr)
+    link = link_to '+', follow_path(usr), class: 'textdec-none circle-link'
     return link if current_user
 
     nil
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
   def unfollow(user)
-    cntnt = link_to 'Unfollow', following_path(user), method: :delete, :class => 'textdec-none form-btn unfollow-btn align-self-center'  
+    cntnt = link_to 'Unfollow', following_path(user), method: :delete, class: 'textdec-none form-btn unfollow-btn align-self-center'
     return cntnt if current_user.follows.include?(user)
   
     nil
@@ -9,30 +9,28 @@ module UsersHelper
   def user_edit_destroy_retweet(opi)
     cntnt = ''
     if current_user && opi.user.username == current_user.username
-      cntnt.concat(link_to image_tag('https://raw.githubusercontent.com/Stricks1/cap-twitter/b3ee28c00f9504f9995a544ad14f2440b83f40a0/app/assets/images/pencil.svg', border: 0, class: 'edit-opinion'), edit_opinion_path(opi))
-      cntnt.concat(link_to 'x', opi, method: :delete, data: { confirm: 'Are you sure?' }, :class => 'textdec-none ml-1')
+      cntnt.concat((link_to image_tag('https://raw.githubusercontent.com/Stricks1/cap-twitter/b3ee28c00f9504f9995a544ad14f2440b83f40a0/app/assets/images/pencil.svg', border: 0, class: 'edit-opinion'), edit_opinion_path(opi)))
+      cntnt.concat((link_to 'x', opi, method: :delete, data: { confirm: 'Are you sure?' }, class: 'textdec-none ml-1'))
       cntnt.html_safe
-    else
-      if current_user && opi.user.username != current_user.username
-        cntnt.concat(link_to image_tag('https://raw.githubusercontent.com/Stricks1/cap-twitter/feature/app/assets/images/retweet.png', border: 0, class: 'edit-opinion'), retweet_path(opi))
-        cntnt.html_safe
-      end
+    elsif current_user && opi.user.username != current_user.username
+      cntnt.concat((link_to image_tag('https://raw.githubusercontent.com/Stricks1/cap-twitter/feature/app/assets/images/retweet.png', border: 0, class: 'edit-opinion'), retweet_path(opi)))
+      cntnt.html_safe
     end
   end
 
-  def btn_follow(u)
-    link = link_to '+', follow_path(u), :class => 'textdec-none circle-link'
-    return link unless current_user.follows.include?(u) || current_user == u
-    
+  def btn_follow(usr)
+    link = link_to '+', follow_path(usr), class: 'textdec-none circle-link'
+    return link unless current_user.follows.include?(usr) || current_user == usr
+
     nil
   end
 
   def user_copied_info(opi)
     return nil unless opi.copied
 
-    edited = opi.created_at == opi.updated_at ? false : true
+    edited = !(opi.created_at == opi.updated_at)
     info = edited ? 'Edited opinion copied from @' : 'Opinion copied from @'
     info.concat(opi.copied.username)
-    cntnt = link_to info, user_path(opi.copied)
+    link_to info, user_path(opi.copied)
   end
 end
